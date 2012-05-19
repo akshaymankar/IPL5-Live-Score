0fc032bf1cca94a094cd37e1caa20a5763e7539e
akshaymankar <itsakshaymankar@gmail.com>
2012-05-19 21:41:53 +0530

    Batting Scorecard Done :)

--- getScore.rb
+++ getScore.rb
@@ -6,6 +6,15 @@ require 'net/http'
 require_relative 'openUriHandleGzip'
 require_relative 'getMatchName'
 
+def getPlayerName(players,id)
+	for player in players
+		if player["id"] == id
+			break
+		end
+	end
+	player["names"][1]
+end
+
 source=openUriHandleGzip('data.iplt20.com','/core/cricket/2012/ipl2012/' + getMatchName() + '/scoring.js')
 score_json=source.sub(/onScoring\(/,'').sub(/\);/,'')
 score=JSON.parse(score_json)
@@ -21,3 +30,18 @@ if !score["innings"][1].nil?
 end
 print "\n"
 print score["matchInfo"]["matchSummary"] + "\n"
+
+if ARGV[0] == "bat"
+	for i in 0..1
+		if score["innings"][i].nil?
+			break
+		end
+		batStat=score["innings"][i]["scorecard"]["battingStats"]
+		print "\n"+score["matchInfo"]["teams"][playing1st]["team"]["fullname"]+"\n"
+		for player in batStat
+			print "#{getPlayerName(score["matchInfo"]["teams"][playing1st]["players"],player["playerId"])}\t #{player["r"]} (#{player["b"]}) #{player["4"]}x4 #{player["6"]}x6\n"
+		end
+	end
+end
+
+
