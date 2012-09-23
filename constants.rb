68480f2fdc3b60ae0f7fe4625bd9623ab98b5043
Akshay Mankar <akshaym@thoughtworks.com>
2012-09-23 22:35:53 +0530

    Constants In Seperate File

---
+++ constants.rb
@@ -0,0 +1,5 @@
+HOST = 'dynamic.pulselive.com'
+BASE_PATH_FOR_TOURNAMENT = '/dynamic/data/core/cricket/2012/worldt20-2012/'
+SCORING_FILE = '/scoring.js'
+MATCH_SCHEDULE_FILE = 'matchSchedule2.js'
+MAX_MATCHES=72
