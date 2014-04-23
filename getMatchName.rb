8fc8070d1e92f274e35ef3c15c05d04d13268cc0
Imran Ahmed <iahmed213@gmail.com>
2014-04-24 00:00:33 +0530

    Updated for IPL7

--- getMatchName.rb
+++ getMatchName.rb
@@ -4,7 +4,7 @@ require 'net/http'
 MAX_MATCHES=72
 
 def getMatchName()
-    source = Net::HTTP.get(URI.parse('http://dynamic.pulselive.com/dynamic/data/core/cricket/2012/ipl2013/matchSchedule2.js'))
+    source = Net::HTTP.get(URI.parse('http://dynamic.pulselive.com/dynamic/data/core/cricket/2012/ipl2014/matchSchedule2.js'))
     sch_json=source.sub(/onMatchSchedule\(/,'').sub!(/\);/,'')
     sch=JSON.parse(sch_json)
     i=0
@@ -13,11 +13,10 @@ def getMatchName()
         #print i.to_s + sch["schedule"][i]["matchState"]+"\n"
         break if i >= MAX_MATCHES
     end
-    
+
     if sch["schedule"][i]["matchState"].eql? "U"
         return sch["schedule"][i-1]["matchId"]["name"]
     else
         return sch["schedule"][i]["matchId"]["name"]
     end
 end
-
